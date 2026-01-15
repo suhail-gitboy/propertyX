3e064a19784a0399cf2898d65d4be5ffdc6f19e6
ameer suhail <suhail@ameers-MacBook-Air.local>
2026-01-15 21:00:05 +0530

    cd

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,13 @@ const allowedOrigins = [
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "*",
+  origin: allowedOrigins,
+
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.options("*", cors());
+app.options("/*", cors());
 // mongoose connection 
 import "./config/db.js"
 import { Authroute } from "./routes/auth.route.js";
